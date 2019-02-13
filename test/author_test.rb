7bed20449306239d97cc674293904d8157836f4c
Carrie Walsh <carriewalsh@gmail.com>
2019-02-13 09:26:01 -0700

    Adds AuthorTest add_books and books

--- test/author_test.rb
+++ test/author_test.rb
@@ -4,6 +4,8 @@ require "./lib/author"
 class AuthorTest < Minitest::Test
   def setup
     @nk_jemisin = Author.new({first_name: "N.K.", last_name: "Jemisin"})
+    @nk_jemisin.add_book("The Fifth Season", "November 3, 2015")
+    @nk_jemisin.add_book("The Hundred Thousand Kingdoms", "2010")
   end
 
 
@@ -19,5 +21,16 @@ class AuthorTest < Minitest::Test
 
   def test_author_has_last_name
     assert_equal "Jemisin", @nk_jemisin.last_name
+  end
+
+
+  def test_author_starts_with_no_books_by_default
+    author = Author.new({first_name: "Namey", last_name: "Name"})
+    assert_equal [], author.books
+  end
+
+
+  def test_author_can_have_books_added
+    assert_equal [@fifth_season, @hundred_thousand_kingdoms], @nk_jemisin.books
   end
 end

c28b22f8428304cc1200fb74ae26c8adf62f54db
Carrie Walsh <carriewalsh@gmail.com>
2019-02-13 09:17:20 -0700

    Cleans up Book/BookTest

--- lib/book.rb
+++ lib/book.rb
@@ -3,11 +3,11 @@ class Book
       :author_last_name,
       :title,
       :publication_date
+      
   def initialize(info)
     @author_first_name = info[:author_first_name]
     @author_last_name = info[:author_last_name]
     @title = info[:title]
     @publication_date = info[:publication_date]
-
   end
 end

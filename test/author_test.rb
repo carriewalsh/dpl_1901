1bd679a295a7e6b34b288f7a83db64eb9f3003f7
Carrie Walsh <carriewalsh@gmail.com>
2019-02-13 09:20:46 -0700

    Adds AuthorTest attributes

--- test/author_test.rb
+++ test/author_test.rb
@@ -1,8 +1,23 @@
 require './test/test_helper'
+require "./lib/author"
 
 class AuthorTest < Minitest::Test
   def setup
-    @author = Author.new
+    @nk_jemisin = Author.new({first_name: "N.K.", last_name: "Jemisin"})
   end
 
+
+  def test_author_exists
+    assert_instance_of Author, @nk_jemisin
+  end
+
+
+  def test_author_has_first_name
+    assert_equal "N.K.", @nk_jemisin.first_name
+  end
+
+
+  def test_author_has_last_name
+    assert_equal "Jemisin", @nk_jemisin.last_name
+  end
 end

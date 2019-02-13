e53b0881950029ab0bc5e494e3c8997bf619ead5
Carrie Walsh <carriewalsh@gmail.com>
2019-02-13 10:28:49 -0700

    Adds catalog method. Makes sure The not included in alphabetizing

--- lib/library.rb
+++ lib/library.rb
@@ -14,4 +14,21 @@ class Library
       book.title == title
     end
   end
+
+  def card_catalog
+    no_the = @books.map do |book|
+      book.title = book.title.split
+      if book.title[0] == "The"
+        book.title.shift
+        book.title.join(" ")
+      end
+      book
+    end
+    titles_only = no_the.sort_by do |book|
+      book.title
+    end
+    titles_only.sort_by do |book|
+      book.author_last_name
+    end
+  end
 end
